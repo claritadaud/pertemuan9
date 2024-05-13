201c3b3415d508cd7dc8dff1e9c2aa53319a6d10
claritadaud <163798943+claritadaud@users.noreply.github.com>
2024-05-13 12:17:04 +0800

    Add files via upload

---
+++ Pertemuan9.java
@@ -0,0 +1,22 @@
+
+package pertemuan9;
+
+/**
+ *
+ * @author Ririn2201010655
+ * tgl = 2024-05-13
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frm01().setVisible(true);
+        frm01 f1 = new frm01();
+        f1.setResizable(false);
+        f1.setAlwaysOnTop(true);
+        f1.setVisible(true);
+    }
+    
+}
